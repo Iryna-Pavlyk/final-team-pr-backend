c9dcc7e25b10314762fb5f98c93fecbd4301e8dc
OksanaBakhaieva <obahaeva@gmail.com>
2024-08-01 15:22:44 +0100

    Create signup, signin, refresh and logout routers

---
+++ src/routers/auth.js
@@ -0,0 +1,14 @@
+import { Router } from 'express';
+import ctrlWrapper from '../utils/ctrlWrapper.js';
+import validateBody from '../utils/validateBody.js';
+import { userSigninSchema, userSignupSchema } from '../validation/user-schemas.js';
+import { signinController, logoutController, refreshController, signupController } from '../controllers/auth-controllers.js';
+
+const authRouter = Router();
+
+authRouter.post('/signup', validateBody(userSignupSchema), ctrlWrapper(signupController));
+authRouter.post('/signin', validateBody(userSigninSchema), ctrlWrapper(signinController));
+authRouter.post('/refresh', ctrlWrapper(refreshController));
+authRouter.post('/logout', ctrlWrapper(logoutController));
+
+export default authRouter;
---
+++ src/validation/user-schemas.js
@@ -0,0 +1,12 @@
+import Joi from 'joi';
+import { emailRegexp } from '../constants/user-constants.js';
+
+export const userSignupSchema = Joi.object({
+    email: Joi.string().pattern(emailRegexp).required(),
+    password: Joi.string().min(6).required(),
+});
+
+export const userSigninSchema = Joi.object({
+    email: Joi.string().pattern(emailRegexp).required(),
+    password: Joi.string().min(6).required(),
+});
