ec8c8e1385a7861da2827ce3f4796e795d3eb004
Iryna-Pavlyk <145845654+Iryna-Pavlyk@users.noreply.github.com>
2024-08-04 19:58:21 +0300

    add docs all routes

--- src/controllers/water.js
+++ src/controllers/water.js
@@ -9,16 +9,20 @@ import createHttpError from 'http-errors';
 export const getDayWaterController = async (req, res, next) => {
   const { date, waterRate } = req.body;
 
-  const {waters, dailyProgress} = await getDayWater({date, waterRate, userId: req.user._id})
+  const { waters, dailyProgress } = await getDayWater({
+    date,
+    waterRate,
+    userId: req.user._id,
+  });
 
   res.status(200).json({
     status: 200,
     message: `Successfully get waters for ${date}`,
-    data: { dailyProgress, waters }
-  })
-}
+    data: { dailyProgress, waters },
+  });
+};
 
-export const getMonthWaterController = async (req, res, next) => {}
+export const getMonthWaterController = async (req, res, next) => {};
 
 export const addWaterController = async (req, res, next) => {
   const { waterAmount, date } = req.body;
