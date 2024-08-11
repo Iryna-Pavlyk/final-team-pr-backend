bf826969d2f80453afd59ad3a972a4b2074f1917
Dmytro Popovskyi <popovskiy96@gmail.com>
2024-08-11 14:52:02 +0200

    add/patch/delet water also retirn updatet daily progress

--- src/controllers/water.js
+++ src/controllers/water.js
@@ -8,7 +8,6 @@ import createHttpError from 'http-errors';
 
 export const getDayWaterController = async (req, res, next) => {
   const {date} = req.query
-  console.log(`user: ${req.user}`);
   const {waters, dailyProgress} = await getDayWater({
     date,
     waterRate: req.user.waterRate,
@@ -51,10 +50,24 @@ export const addWaterController = async (req, res, next) => {
     date,
   });
 
+  if (!water) {
+    // 409 checks by Joi and model. This is just extra protection in case to give more detailed message than Something going wrong
+    throw createHttpError(500, `Failed to add water`);
+  }
+
+  const {dailyProgress} = await getDayWater({
+    date: water.date.slice(0, water.date.indexOf('T')),
+    waterRate: req.user.waterRate,
+    userId: req.user._id
+  })
+
   res.status(201).json({
     status: 201,
     message: `Successfully created a water!`,
-    data: water,
+    data: {
+      updatedDailyProgress: dailyProgress,
+      water
+    },
   });
 };
 
@@ -69,27 +82,49 @@ export const deleteWaterController = async (req, res, next) => {
     return;
   }
 
-  res.status(204).send();
+  const {dailyProgress} = await getDayWater({
+    date: water.date.slice(0, water.date.indexOf('T')),
+    waterRate: req.user.waterRate,
+    userId: req.user._id
+  })
+
+  res.status(200).json({
+    status: 200,
+    message: `Successfully deleted water!`,
+    data: {
+      updatedDailyProgress: dailyProgress,
+      water
+    }
+  });
 };
 
 export const patchWaterController = async (req, res, next) => {
   const { waterId } = req.params;
   const userId = req.user._id;
 
-  const result = await updateWater({
+  const water = await updateWater({
     waterId,
     userId,
     payload: { ...req.body },
   });
 
-  if (!result) {
+  if (!water) {
     next(createHttpError(404, 'Water not found'));
     return;
   }
 
+  const {dailyProgress} = await getDayWater({
+    date: water.date.slice(0, water.date.indexOf('T')),
+    waterRate: req.user.waterRate,
+    userId: req.user._id
+  })
+
   res.json({
     status: 200,
-    message: 'Successfully patched a water!',
-    data: result.water,
+    message: 'Successfully update a water!',
+    data: {
+      updatedDailyProgress: dailyProgress,
+      water
+    }
   });
 };
