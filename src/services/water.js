14ce8eddef0b4a4d3d9e69dfe3975892ae2d26c8
Dmytro Popovskyi <popovskiy96@gmail.com>
2024-08-11 18:39:34 +0200

    add sumWaterAmount field for waterMoth response'

--- src/services/water.js
+++ src/services/water.js
@@ -53,12 +53,16 @@ export const getMonthWater = async ({date, waterRate, userId}) => {
 
   const results = Array.from({ length: numberOfDays }, (_, i) => ({
     date: `${date}-${String(i + 1).padStart(2, '0')}`,
-    dailyProgress: 0
+    dailyProgress: 0,
+    sumWaterAmount: 0
   }));
 
   waters.forEach(water => {
     const index = Number(water._id.slice(-2)) - 1
     results[index].dailyProgress = calculateWaterProgress(waterRate, water.waters)
+    results[index].sumWaterAmount = water.waters.reduce((sum, {waterAmount}) => {
+      return sum + waterAmount;
+    }, 0)
   })
 
   return results
